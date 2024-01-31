65b91d87b459a018917de9d6c0b659d90df4ab5d
unknown <amalsurendran270@gmail.com>
2024-01-31 10:45:44 +0530

    login page update

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -50,7 +50,7 @@ function App() {
             <div className="text-dark px-3 py-4 p-md-5 mx-md-4">
               <h4 className="mb-4">We are more than just a company</h4>
               <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                tempor incididunt ut labore et dolqua. Ut enim ad minim veniam, quis nostrud
                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               </p>
             </div>
